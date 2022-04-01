a5bb5a0fa65657977af37e1141c835dba0a2cebb
edreton <edgar.retontali@gmail.com>
2022-03-31 18:12:35 -0600

    first commit

---
+++ components/Map.js
@@ -0,0 +1,47 @@
+import { useEffect, useContext } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { UberContext } from '../context/uberContext'
+
+const style = {
+    wrapper: `flex-1 h-full w-full`,
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
+const Map = () => {
+    const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+
+    console.log(pickupCoordinates, dropoffCoordinates)
+
+    useEffect(() => { 
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-74.50, 40],
+            zoom: 9
+        })
+        if (pickupCoordinates) {
+            addToMap(map, pickupCoordinates)
+          }
+      
+          if (dropoffCoordinates) {
+            addToMap(map, dropoffCoordinates)
+          }
+      
+          if (pickupCoordinates && dropoffCoordinates) {
+            map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+              padding: 400,
+            })
+          }
+    }, [pickupCoordinates, dropoffCoordinates])
+
+    const addToMap = (map, coordinates) => {
+        const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+      }
+
+    return (
+        <div className={style.wrapper} id='map' />
+    )
+}
+
+export default Map
